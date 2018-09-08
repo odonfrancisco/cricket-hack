076d099bb5535547defd5110148edd0165877170
fodon11 <francisco.odon11@gmail.com>
2018-09-08 15:05:30 -0400

    Added buy-now component and landing info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavBarComponent } from './landing-page/nav-bar/nav-bar.component';
+import { NutritionalComponent } from './landing-page/nutritional/nutritional.component';
+import { BuyNowComponent } from './landing-page/buy-now/buy-now.component';
+import { LandingContentComponent } from './landing-page/landing-content/landing-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    NavBarComponent
+    NavBarComponent,
+    NutritionalComponent,
+    BuyNowComponent,
+    LandingContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/landing-page/nutritional/nutritional.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nutritional',
+  templateUrl: './nutritional.component.html',
+  styleUrls: ['./nutritional.component.css']
+})
+export class NutritionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
