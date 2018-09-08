8d0eeabe39869d94e845703c3280c2ec528691bb
fodon11 <francisco.odon11@gmail.com>
2018-09-08 14:22:47 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavBarComponent } from './landing-page/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
